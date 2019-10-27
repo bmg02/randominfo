ea054a126d6c1e7469c9405e0248c936e5a899af
Bhuvan Gandhi <bhuvan12501@gmail.com>
2019-10-27 22:23:50 +0530

    Updated on 27-10-19 10:30 PM

--- randominfo/__init__.py
+++ randominfo/__init__.py
@@ -2,7 +2,9 @@ from __future__ import unicode_literals
 import sys, os
 sys.path.append("/randominfo/")
 from .Date_Time import Date_Time
-from .Person_Name import Person_Name
+from .Person import Person
+from random import randint, choice, sample
+from datetime import datetime
 
 
 __title__ = 'randominfo'
@@ -20,8 +22,96 @@ except Exception as e:
     else:
         print(e)
 
+def get_digit_otp(len):
+    otp = ""
+    for _ in range(len):
+        otp += str(randint(1,9))
+    return otp
 
-'''REFERENCE:
+def get_chars_otp(len, lowerCase = True, upperCase = True):
+    otp = ""
+    chars = ""
+    if lowerCase == True:
+        if upperCase == True:
+            chars = "QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm"
+        else:
+            chars = "qwertyuioplkjhgfdsazxcvbnm"
+    else:
+        if upperCase == True:
+            chars = "QWERTYUIOPLKJHGFDSAZXCVBNM"
+        else:
+            return "Invalid combination."
+    for _ in range(len):
+        otp += str(chars[randint(0, len(chars) - 1)])
+    return otp
+
+def get_otp(len):
+    otp = ""
+    chars = "QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm0123456789"
+    for _ in range(len):
+        otp += str(chars[randint(0, len(chars) - 1)])
+    return otp
+
+def get_formatted_datetime(strDate, _format = "%d-%m-%Y %H:%M:%S"):
+    return datetime.strptime(strDate, _format)
+
+def get_email():
+    domains = ["gmail", "yahoo", "hotmail", "express", "yandex", "nexus", "online", "omega", "institute", "finance", "company", "corporation", "community"]
+    extentions = ['com', 'in', 'jp', 'us', 'uk', 'org', 'edu', 'au', 'de', 'co', 'me', 'biz', 'dev', 'ngo', 'site', 'xyz', 'zero', 'tech']
+    if Person == None:
+        prsn = Person()
+    else:
+        prsn = Person
+
+    c = randint(0,2)
+    dmn = choice(domains)
+    ext = choice(extentions)
+
+    if c == 0:
+        email = prsn.first_name + get_formatted_datetime(prsn.birthdate(), "%Y") + dmn + "." + ext
+    elif c == 1:
+        email = prsn.last_name + get_formatted_datetime(prsn.birthdate(), "%d") + dmn + "." + ext
+    else:
+        email = prsn.first_name + get_formatted_datetime(prsn.birthdate(), "%y") + dmn + "." + ext
+    return email
+
+def random_password(length = 8, special_chars = True, digits = True):
+    spec_chars = ['!', '@', '#', '$', '%', '^', '&', '*']
+    alpha = "QWERTYUIOPLKJHGFDSAZXCVBNMmnbvcxzasdfghjklpoiuytrewq"
+    spec_char_len = 0
+    chars = ""
+    if special_chars == True:
+        spec_char_len = randint(1,3)
+        for _ in range(spec_char_len):
+            chars += choice(spec_chars)
+
+    if digits == True:
+        spec_char_len = randint(0,9)
+        for _ in range(spec_char_len):
+            chars += str(randint(0,9))
+
+    for _ in range(length - spec_char_len):
+        chars += choice(alpha[randint(0, len(alpha) - 1)])
+
+    paswd = ''.join(sample(chars, len(chars)))
+    return paswd
+
+def get_phone_number(country_code = True):
+    phone = ""
+    if country_code == True:
+        cCodes = [91, 144, 141, 1, 44, 86, 52, 61, 32, 20, 33, 62, 81, 31, 7]
+        phone = "+"
+        phone += str(choice(cCodes))
+        phone += " "
+    for i in range(0,10):
+        if i == 0:
+            phone += str(randint(8,9))
+        else:
+            phone += str(randint(0,9))
+    return phone
+
+'''
+REFERENCE:
 http://www.first-names-meanings.com/country-indian-names.html
 https://www.familyeducation.com/baby-names/browse-origin/surname/indian
 '''
