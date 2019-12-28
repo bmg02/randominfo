6eee918a1fb8b1b1a78aef5ac025494de3e18d3f
Bhuvan Gandhi <bhuvan12501@gmail.com>
2019-12-28 17:00:11 +0530

    Updated on 28-12-19

--- setup.py
+++ setup.py
@@ -13,9 +13,10 @@ setuptools.setup(
     author="Bhuvan Gandhi",
     author_email="bhuvan12501@gmail.com",
     description="Random data generator for IDs, names, emails, passwords, dates, numbers, addresses, images, OTPs etc. for dummy entries.",
-    long_description=long_description,
-    url="https://github.com/bmg02/randominfo.git",
+    url="https://github.com/bmg02/randominfo",
+    download_url = 'https://github.com/bmg02/randominfo/dist/randominfo-0.1.tar.gz',
     packages=setuptools.find_packages(),
+    install_requires = ["PIL", "csv", "pytz", "glob"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
